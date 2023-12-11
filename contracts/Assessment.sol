67859fc724afbeee466043cc9909882aa0905b1b
prathapsingh924 <152711011+prathapsingh924@users.noreply.github.com>
2023-12-11 14:50:35 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,46 +1,51 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    uint256 public totalLoans;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event TakeLoan(uint256 amount, uint256 interestRate, uint256 duration);
+    event RepayLoan(uint256 amount);
 
-    constructor(uint initBalance) payable {
+    struct Loan {
+        uint256 amount;
+        uint256 interestRate; // Annual interest rate in percentage
+        uint256 duration; // Loan duration in months
+        uint256 startTime;
+    }
+
+    mapping(address => Loan) public loans;
+
+    constructor(uint256 initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
     }
 
-    function getBalance() public view returns(uint256){
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
     function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
 
-        // make sure this is the owner
         require(msg.sender == owner, "You are not the owner of this account");
 
-        // perform transaction
         balance += _amount;
 
-        // assert transaction completed successfully
         assert(balance == _previousBalance + _amount);
 
-        // emit the event
         emit Deposit(_amount);
     }
 
-    // custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
         require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
+        uint256 _previousBalance = balance;
         if (balance < _withdrawAmount) {
             revert InsufficientBalance({
                 balance: balance,
@@ -48,13 +53,53 @@ contract Assessment {
             });
         }
 
-        // withdraw the given amount
         balance -= _withdrawAmount;
 
-        // assert the balance is correct
         assert(balance == (_previousBalance - _withdrawAmount));
 
-        // emit the event
         emit Withdraw(_withdrawAmount);
+    }
+
+    function takeLoan(uint256 _amount, uint256 _interestRate, uint256 _duration) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(loans[msg.sender].amount == 0, "You already have an active loan");
+
+        uint256 interestAmount = (_amount * _interestRate * _duration) / (100 * 12);
+
+        loans[msg.sender] = Loan({
+            amount: _amount,
+            interestRate: _interestRate,
+            duration: _duration,
+            startTime: block.timestamp
+        });
+
+        balance += _amount - interestAmount;
+        totalLoans += _amount;
+
+        emit TakeLoan(_amount, _interestRate, _duration);
+    }
+
+    function repayLoan(uint256 _amount) public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        require(loans[msg.sender].amount > 0, "No active loan to repay");
+
+        Loan storage loan = loans[msg.sender];
+
+        // Calculate interest accrued
+        uint256 elapsedTime = (block.timestamp - loan.startTime) / (30 days);
+        uint256 interestAccrued = (loan.amount * loan.interestRate * elapsedTime) / (100 * 12);
+
+        uint256 totalRepayment = loan.amount + interestAccrued;
+
+        require(_amount <= totalRepayment, "Invalid repayment amount");
+
+        // Update balance and total loans
+        balance -= _amount;
+        totalLoans -= loan.amount;
+
+        // Clear the loan details
+        delete loans[msg.sender];
+
+        emit RepayLoan(_amount);
     }
 }
